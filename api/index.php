d3008a9e9c54f6057ec3234042b9d5d1d4aa29cc
Adeeva Rashida <rashidaadeeva@gmail.com>
2026-05-30 21:00:58 +0700

    Ubah storage biar vercelnya work

--- api/index.php
+++ api/index.php
@@ -1,2 +1,8 @@
 <?php
+
+$compiledViewPath = '/tmp/storage/framework/views';
+if (!is_dir($compiledViewPath)) {
+    mkdir($compiledViewPath, 0755, true);
+}
+
 require __DIR__ . '/../public/index.php';
