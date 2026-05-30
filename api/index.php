217ed796145e266ef32bc677947a2184dbd38576
Adeeva Rashida <rashidaadeeva@gmail.com>
2026-05-30 21:11:54 +0700

    Fix Laravel 12 storage path for Vercel

--- api/index.php
+++ api/index.php
@@ -1,8 +1,25 @@
 <?php
 
-$compiledViewPath = '/tmp/storage/framework/views';
-if (!is_dir($compiledViewPath)) {
-    mkdir($compiledViewPath, 0755, true);
+$storagePath = '/tmp/storage';
+
+$directories = [
+    $storagePath . '/app',
+    $storagePath . '/framework/cache/data',
+    $storagePath . '/framework/sessions',
+    $storagePath . '/framework/views',
+    $storagePath . '/logs',
+];
+
+foreach ($directories as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
 }
+
+$_ENV['APP_STORAGE'] = $storagePath;
+putenv('APP_STORAGE=' . $storagePath);
+
+$_ENV['VIEW_COMPILED_PATH'] = $storagePath . '/framework/views';
+putenv('VIEW_COMPILED_PATH=' . $storagePath . '/framework/views');
 
 require __DIR__ . '/../public/index.php';
